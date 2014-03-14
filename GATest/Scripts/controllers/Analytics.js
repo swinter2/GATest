6fc4d705e5ef5f5b91fe94135d43750d9e575b02
Sam Winter <sam.winter@silvertech.com>
2014-03-14 16:41:03 -0400

    Changes.

--- GATest/Scripts/controllers/Analytics.js
+++ GATest/Scripts/controllers/Analytics.js
@@ -11,33 +11,42 @@ var $consoleTextarea = null;
 function log(message) {
     console.log(message);
 
-    ngScope().$apply(function ($s) {
-        var now = new Date();
-        message = now.logFormat() + "> " + message;
-        $s.output += message + "\n";
-    });
+    if (typeof (message) === 'string') {
+        ngScope().$apply(function ($s) {
+            var now = new Date();
+            message = now.logFormat() + "> " + message;
+            $s.output += message + "\n";
+        });
 
-    if ($consoleTextarea) {
-        var t = $consoleTextarea[0];
-        t.scrollTop = t.scrollHeight - t.clientHeight;
+        if ($consoleTextarea) {
+            var t = $consoleTextarea[0];
+            t.scrollTop = t.scrollHeight - t.clientHeight;
+        }
     }
 }
 
 function showError(message) {
     console.log(message);
 
-    ngScope().$apply(function ($s) {
-        $s.error = message;
-        var now = new Date();
-        $s.output += now.logFormat() + "> " + message + "\n";
-    });
+    if (typeof (message) === 'string') {
+        ngScope().$apply(function($s) {
+            $s.error = message;
+            var now = new Date();
+            $s.output += now.logFormat() + "> " + message + "\n";
+        });
+
+        if ($consoleTextarea) {
+            var t = $consoleTextarea[0];
+            t.scrollTop = t.scrollHeight - t.clientHeight;
+        }
+    }
 }
 
 gaApp.controller("Analytics",
     function Analytics($scope) {
-        $scope.clientId = "";
-        $scope.apiKey = "";
-        $scope.trackingCode = "";
+        $scope.clientId = "955069224494.apps.googleusercontent.com";
+        $scope.apiKey = "AIzaSyDdU5uKPYj0zX6XGHDQ5RY-O9wS_OM7HDc";
+        $scope.trackingCode = "UA-2003853-6";
         $scope.accounts = null;
         $scope.accountId = "";
         $scope.username = "";
@@ -55,17 +64,30 @@ jQuery(function ($) {
     $consoleTextarea = $("#console textarea");
 
     var $datepicker = $(".datepicker");
-    $datepicker.datepicker();
+    $datepicker.datepicker({
+        onSelect: function (dateText) {
+            log(this);
+            log(dateText);
+            var id = this.id;
+            switch (id) {
+                case "startDate":
+                    break;
+                case "endDate":
+                    break;
+            }
+            ngScope().$apply(function ($s) {
+
+            });
+        }
+    });
 
     $datepicker.each(function (i, dp) {
         $(dp).val($(dp).data('value'));
     });
 
     $("#fetchResults").on('click', function (e) {
-        var $scope = ngScope();
-        $scope.error = "";
-
-        $scope.$apply(function ($s) {
+        ngScope().$apply(function ($s) {
+            $s.error = "";
             $s.results = null;
         });
         makeApiCall();
@@ -73,7 +95,8 @@ jQuery(function ($) {
 
     $("#form").on("click", "label.account input", function (e) {
         var val = $(this).val();
-        log(val);
+        var name = $(this).data('name');
+        log("Switching accounts: " + name + "(id: " + val + ")");
         ngScope().$apply(function ($s) {
             $s.accountId = val;
         });
